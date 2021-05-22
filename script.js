0bb18dddc6f89d8e6c8663166df501c303f832e3
David Coelho <david.guedes@bolttech.io>
2021-05-22 19:18:29 -0300

    update element's size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 let data = [];
 // const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
-const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+const letras = ['a', 'b', 'c', 'd', 'e'];
 const size = letras.length;
 
 for (let x = 0; x < size; x++) {
